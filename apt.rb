314e0a689f17024b1ee5a8fcbde125d2fa88535d
Andrew Beresford <beezly@beezly.org.uk>
2013-02-27 12:29:14 +0000

    refactor standard apt methods

--- apt.rb
+++ apt.rb
@@ -32,29 +32,29 @@ module MCollective
         reply.data[:packages] = waiting_updates
       end
       
-      def update_action
-        reply[:status] = run("apt-get -q update", :stdout => :out, :stderr => :err, :environment => {'DEBCONF_FRONTEND' => 'noninteractive'})
+      def standard_apt_action action
+        reply[:status] = run(action, :stdout => :out, :stderr => :err, :environment => {'DEBCONF_FRONTEND' => 'noninteractive'})
         reply.fail "Error",1 unless reply[:status] == 0
+      end
+      
+      def update_action
+        standard_apt_action "apt-get -q update"
       end
       
       def upgrade_action
-        reply[:status] = run("apt-get -q -y upgrade", :stdout => :out, :stderr => :err, :environment => {'DEBCONF_FRONTEND' => 'noninteractive'})
-        reply.fail "Error",1 unless reply[:status] == 0
+        standard_apt_action "apt-get -q -y upgrade"
       end
       
       def distupgrade_action
-        reply[:status] = run("apt-get -q -y dist-upgrade", :stdout => :out, :stderr => :err, :environment => {'DEBCONF_FRONTEND' => 'noninteractive'})
-        reply.fail "Error",1 unless reply[:status] == 0
+        standard_apt_action "apt-get -q -y dist-upgrade"
       end
       
       def install_action
-        reply[:status] = run("apt-get -q -y install #{request.data[:package]}", :stdout => :out, :stderr => :err, :environment => {'DEBCONF_FRONTEND' => 'noninteractive'})
-        reply.fail "Error",1 unless reply[:status] == 0
+        standard_apt_action "apt-get -q -y install #{request.data[:package]}"
       end
       
       def remove_action
-        reply[:status] = run("apt-get -q -y remove #{request.data[:package]}", :stdout => :out, :stderr => :err, :environment => {'DEBCONF_FRONTEND' => 'noninteractive'})
-        reply.fail "Error",1 unless reply[:status] == 0
+        standard_apt_action "apt-get -q -y remove #{request.data[:package]}"
       end
     end
   end
