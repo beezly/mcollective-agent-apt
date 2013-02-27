d780a24eb3e2ba64326e41b9bbb84447038f618d
Andrew Beresford <beezly@beezly.org.uk>
2013-02-27 07:52:02 +0000

    Add update, upgrade, distupgrade, install and remove commands

--- apt.ddl
+++ apt.ddl
@@ -13,3 +13,37 @@ action "get_upgrades", :description => "Get the total number of packages waiting
     :description => "Packages available to upgrade",
     :display_as => "Packages available to upgrade"
 end
+
+["update", "upgrade", "distupgrade"].each do |act|
+  action act, :description => "Executes an apt-get #{act}" do
+    display :always
+
+    output :out, 
+      :description => "Output of apt-get #{act}",
+      :display_as  => "Command output"
+
+	output :err,
+	  :description => "Errors from apt-get #{act}",
+	  :display_as  => "Errors"
+  end
+end
+
+["install", "remove"].each do |act|
+  action act, :description => "#{act.capitalize} a package" do
+    input :package,
+      :prompt		 => 'Package Name',
+	  :description   => "The package to #{act}",
+	  :type          => :string,
+	  :validation	 => '^[a-zA-Z\-_\d]+$',
+	  :maxlength     => 100,
+	  :optional      => false
+	
+	output :out,
+	  :description => "Output of apt-get #{act}",
+	  :display_as  => "Command output"
+	
+	output :err,
+	  :description => "Errors from apt-get #{act}",
+	  :display_as  => "Errors"
+  end	
+end
--- apt.rb
+++ apt.rb
@@ -27,6 +27,31 @@ module MCollective
       def get_upgrades_action
         reply.data[:packages] = waiting_updates
       end
+      
+      def update_action
+        reply[:status] = run("apt-get -q update", :stdout => :out, :stderr => :err, :environment => {'DEBCONF_FRONTEND' => 'noninteractive'})
+        reply.fail "Error",1 unless reply[:status] == 0
+      end
+      
+      def upgrade_action
+        reply[:status] = run("apt-get -q -y upgrade", :stdout => :out, :stderr => :err, :environment => {'DEBCONF_FRONTEND' => 'noninteractive'})
+        reply.fail "Error",1 unless reply[:status] == 0
+      end
+      
+      def distupgrade_action
+        reply[:status] = run("apt-get -q -y dist-upgrade", :stdout => :out, :stderr => :err, :environment => {'DEBCONF_FRONTEND' => 'noninteractive'})
+        reply.fail "Error",1 unless reply[:status] == 0
+      end
+      
+      def install_action
+        reply[:status] = run("apt-get -q -y install #{request.data[:package]}", :stdout => :out, :stderr => :err, :environment => {'DEBCONF_FRONTEND' => 'noninteractive'})
+        reply.fail "Error",1 unless reply[:status] == 0
+      end
+      
+      def remove_action
+        reply[:status] = run("apt-get -q -y remove #{request.data[:package]}", :stdout => :out, :stderr => :err, :environment => {'DEBCONF_FRONTEND' => 'noninteractive'})
+        reply.fail "Error",1 unless reply[:status] == 0
+      end
     end
   end
 end
